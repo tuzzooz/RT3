4b1b140bb784d2eb0a49f88be8ba15a95b3808dd
Otavio Vianna <otavioviannalima@gmail.com>
2025-04-10 20:30:11 -0300

    commit inicial

---
+++ src/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const RT3_1 = require("../src/modules/RT3");
+const somar = new RT3_1.Somador();
+const somado = somar.calcular(3, 2);
+const subtrair = new RT3_1.Subtrador();
+const subtraido = subtrair.calcular(5, 4);
+const multiplicar = new RT3_1.Multiplicador();
+const multiplicado = multiplicar.calcular(2, 8);
+const dividir = new RT3_1.Divisor();
+const dividido = dividir.calcular(9, 3);
+console.log(`3 + 2 dará: ${somado}`);
+console.log(`5 - 4 dará: ${subtraido}`);
+console.log(`2 * 8 dará: ${multiplicado}`);
+console.log(`9 / 3 dará: ${dividido}`);
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { Somador, Subtrador, Multiplicador, Divisor } from "../libs/submodulo/CalculadoraClass/operacoesmatematicas";
+
+const somar = new Somador()
+const somado = somar.calcular(3,2)
+
+const subtrair = new Subtrador()
+const subtraido = subtrair.calcular(5,4)
+
+const multiplicar = new Multiplicador()
+const multiplicado = multiplicar.calcular(2,8)
+
+const dividir = new Divisor()
+const dividido = dividir.calcular(9,3)
+
+console.log(`3 + 2 dará: ${somado}`)
+console.log(`5 - 4 dará: ${subtraido}`)
+console.log(`2 * 8 dará: ${multiplicado}`)
+console.log(`9 / 3 dará: ${dividido}`)
